eff82fea05f65d3f20425e51a451ea090d88bfe7
Maikel van den Boogerd <maikelboogerd@gmail.com>
2019-04-08 22:16:41 +0200

    World > Level > Grid > Blocks

--- Humble/Game/Level.cs
+++ Humble/Game/Level.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,55 @@ namespace Humble
 {
     public class Level
     {
+        private Random random = new Random();
+        private Grid grid;
+
         public Level()
         {
+        }
+
+        /// Generation
+        /// 
+
+        public void Generate()
+        {
+            grid = new Grid(10, 10);
+            grid.Fill();
+        }
+
+        /// Spawning
+        /// 
+
+        public Vector2 getSpawnPoint()
+        {
+            // Get a random block's center point as spawn location.
+            IsometricBlock spawnBlock = grid.blocks[random.Next(grid.blocks.Count())];
+            return spawnBlock.Center();
+        }
+
+        /// Collision
+        /// 
+
+        public bool Intersects(Rectangle rectangle)
+        {
+            foreach (IsometricBlock block in grid.blocks)
+            {
+                Vector2 point = new Vector2(rectangle.X + rectangle.Width / 2, rectangle.Y + rectangle.Height / 2);
+                if (block.Intersects(point))
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// Draw
+        /// 
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw each isometric block by calling it's Draw().
+            foreach (IsometricBlock block in grid.blocks)
+                block.Draw(spriteBatch);
         }
     }
 }
--- Humble/Game/Player.cs
+++ Humble/Game/Player.cs
@@ -131,7 +131,7 @@ namespace Humble
             standUp = new Animation();
             standUp.AddFrame(new Rectangle(0, 0, 64, 64), TimeSpan.FromSeconds(.25));
 
-            changePosition(game.worldController.GetWorld().spawnPoint());
+            changePosition(game.worldController.GetWorld().spawnPoint);
 
             base.Initialize();
         }
