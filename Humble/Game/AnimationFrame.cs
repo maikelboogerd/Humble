6e4795533aac7cc8d61111cefaa78af759bd9982
Tyler <tylerdegraaff@gmail.com>
2019-04-08 19:24:33 +0200

    Add new player spritesheet

---
+++ Humble/Game/AnimationFrame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Humble
+{
+    public class AnimationFrame
+    {
+        public Rectangle SourceRectangle { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
