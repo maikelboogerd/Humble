0853d87b3c2b7aa535da5a8ea901e9f49c26372c
Maikel van den Boogerd <maikelboogerd@gmail.com>
2019-04-11 22:56:50 +0200

    Update Shape.cs

--- Humble/Game/Helpers/Shape.cs
+++ Humble/Game/Helpers/Shape.cs
@@ -26,10 +26,18 @@ namespace Humble
             };
             innerPoints = new List<Vector2>()
             {
-                new Vector2(outerRectangle.X + (outerRectangle.Width / 2), outerRectangle.Y),
-                new Vector2(outerRectangle.X + outerRectangle.Width, outerRectangle.Y + (outerRectangle.Height / 2)),
-                new Vector2(outerRectangle.X + (outerRectangle.Width / 2), outerRectangle.Y + outerRectangle.Height),
-                new Vector2(outerRectangle.X, outerRectangle.Y + (outerRectangle.Height / 2)),
+                //new Vector2(outerRectangle.X + (outerRectangle.Width / 2), outerRectangle.Y),
+                //new Vector2(outerRectangle.X + outerRectangle.Width, outerRectangle.Y + (outerRectangle.Height / 2)),
+                //new Vector2(outerRectangle.X + (outerRectangle.Width / 2), outerRectangle.Y + outerRectangle.Height),
+                //new Vector2(outerRectangle.X, outerRectangle.Y + (outerRectangle.Height / 2)),
+                new Vector2(10, 0) * 5,
+                new Vector2(20, 0) * 5,
+                new Vector2(30, 10) * 5,
+                new Vector2(30, 20) * 5,
+                new Vector2(20, 30) * 5,
+                new Vector2(10, 30) * 5,
+                new Vector2(0, 20) * 5,
+                new Vector2(0, 10) * 5,
             };
         }
 
@@ -40,27 +48,71 @@ namespace Humble
 
         public bool Contains(Vector2 player)
         {
-            Vector2 vectorA1 = new Vector2(player.X - innerPoints[0].X, player.Y - innerPoints[0].Y);
-            Vector2 vectorA2 = new Vector2(player.X - innerPoints[1].X, player.Y - innerPoints[1].Y);
-            Vector2 vectorA3 = new Vector2(player.X - innerPoints[2].X, player.Y - innerPoints[2].Y);
-            Vector2 vectorA4 = new Vector2(player.X - innerPoints[3].X, player.Y - innerPoints[3].Y);
+            // Define vectorA list
+            List<Vector2> vectorA = new List<Vector2>();
+            for (int i = 0; i < innerPoints.Count; i++)
+            {
+                Vector2 vectorAx = new Vector2(player.X - innerPoints[i].X, player.Y - innerPoints[i].Y);
+                vectorA.Add(vectorAx);
+            }
 
-            Vector2 vectorB1 = new Vector2(1, 1);
-            Vector2 vectorB2 = new Vector2(-1, 1);
-            Vector2 vectorB3 = new Vector2(-1, -1);
-            Vector2 vectorB4 = new Vector2(1, -1);
+            // Define vectorB list
+            List<Vector2> vectorB = new List<Vector2>();
+            for (int i = 0; i < innerPoints.Count; i++)
+            {
+                Vector2 vectorBx = new Vector2();
+                Vector2 nextInnerpoint;
 
-            int dotProduct1 = (int)(vectorA1.X * vectorB1.X + vectorA1.Y * vectorB1.Y);
-            int dotProduct2 = (int)(vectorA2.X * vectorB2.X + vectorA2.Y * vectorB2.Y);
-            int dotProduct3 = (int)(vectorA3.X * vectorB3.X + vectorA3.Y * vectorB3.Y);
-            int dotProduct4 = (int)(vectorA4.X * vectorB4.X + vectorA4.Y * vectorB4.Y);
-            List<int> numArray = new List<int>();
-            numArray.Add(dotProduct1);
-            numArray.Add(dotProduct2);
-            numArray.Add(dotProduct3);
-            numArray.Add(dotProduct4);
+                Vector2 actualInnerpoint = innerPoints[i];
+                int stepX = 0;
+                int stepY = 0;
 
-            bool containsNegative = numArray.Any(i => i < 0);
+                if (i + 1 >= innerPoints.Count) {
+                    nextInnerpoint = innerPoints[0];
+                } else {
+                    int next = i + 1;
+                    nextInnerpoint = innerPoints[next];
+                }
+
+                float difX = nextInnerpoint.X - actualInnerpoint.X;
+                float difY = nextInnerpoint.Y - actualInnerpoint.Y;
+
+                if (difX > 0)
+                {
+                    stepX = 1;
+                }
+                else if (difX < 0)
+                {
+                    stepX = -1;
+                }
+                else {
+                    stepX = 0;
+                }
+
+                if (difY > 0)
+                {
+                    stepY = 1;
+                }
+                else if (difY < 0)
+                {
+                    stepY = -1;
+                }
+                else {
+                    stepY = 0;
+                }
+                vectorBx = new Vector2(stepX, stepY);
+                vectorB.Add(vectorBx);
+            }
+
+            List<int> dotProducts = new List<int>();
+
+            for (int i = 0; i < vectorB.Count; i++)
+            {
+                int dotProduct = (int)(vectorA[i].X * vectorB[i].X + vectorA[i].Y * vectorB[i].Y);
+                dotProducts.Add(dotProduct);
+            }
+
+            bool containsNegative = dotProducts.Any(i => i < 0);
 
             if (containsNegative == true)
             {
@@ -87,12 +139,17 @@ namespace Humble
 
             Texture2D pointTexture = new Texture2D(GraphicsDevice, 1, 1);
             pointTexture.SetData(new[] { Color.Yellow });
+
+            foreach (Vector2 point in innerPoints)
+            {
+                spriteBatch.Draw(pointTexture, new Rectangle((int)point.X, (int)point.Y, 1, 1), Color.White);
+            }
             //spriteBatch.Draw(pointTexture, new Rectangle((int)innerPoints[0].X, (int)innerPoints[0].Y, 1, 1), Color.White);
             //spriteBatch.Draw(pointTexture, new Rectangle((int)innerPoints[1].X, (int)innerPoints[1].Y, 1, 1), Color.White);
             //spriteBatch.Draw(pointTexture, new Rectangle((int)innerPoints[2].X, (int)innerPoints[2].Y, 1, 1), Color.White);
             //spriteBatch.Draw(pointTexture, new Rectangle((int)innerPoints[3].X, (int)innerPoints[3].Y, 1, 1), Color.White);
-            spriteBatch.Draw(pointTexture, new Rectangle(0, 0, 1, 1), Color.White);
-            spriteBatch.Draw(pointTexture, new Rectangle(90, 90, 1, 1), Color.White);
+            //spriteBatch.Draw(pointTexture, new Rectangle(0, 0, 1, 1), Color.White);
+            //spriteBatch.Draw(pointTexture, new Rectangle(90, 90, 1, 1), Color.White);
         }
     }
 }
