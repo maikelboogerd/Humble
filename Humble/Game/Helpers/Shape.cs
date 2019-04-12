b78899c769da37cd461c5b3eae3e3f77e507b859
Tyler de Graaff <tyler@labela.nl>
2019-04-13 00:09:49 +0200

    The moment we realized it won't work

--- Humble/Game/Helpers/Shape.cs
+++ Humble/Game/Helpers/Shape.cs
@@ -10,7 +10,7 @@ namespace Humble
 {
     public class Shape
     {
-        private Rectangle outerRectangle = new Rectangle(0, 0, 90, 90);
+        private Rectangle outerRectangle = new Rectangle(0, 0, 250, 250);
 
         private List<Vector2> outerPoints;
         private List<Vector2> innerPoints;
@@ -19,17 +19,19 @@ namespace Humble
         {
             outerPoints = new List<Vector2>()
             {
-                new Vector2(outerRectangle.X, outerRectangle.Y),
-                new Vector2(outerRectangle.X + outerRectangle.Width, outerRectangle.Y),
-                new Vector2(outerRectangle.X, outerRectangle.Y + outerRectangle.Height),
-                new Vector2(outerRectangle.X + outerRectangle.Width, outerRectangle.Y + outerRectangle.Height),
+                //new Vector2(outerRectangle.X, outerRectangle.Y),
+                //new Vector2(outerRectangle.X + outerRectangle.Width, outerRectangle.Y),
+                //new Vector2(outerRectangle.X, outerRectangle.Y + outerRectangle.Height),
+                //new Vector2(outerRectangle.X + outerRectangle.Width, outerRectangle.Y + outerRectangle.Height),
             };
             innerPoints = new List<Vector2>()
             {
-                new Vector2(outerRectangle.X + (outerRectangle.Width / 2), outerRectangle.Y),
-                new Vector2(outerRectangle.X + outerRectangle.Width, outerRectangle.Y + (outerRectangle.Height / 2)),
-                new Vector2(outerRectangle.X + (outerRectangle.Width / 2), outerRectangle.Y + outerRectangle.Height),
-                new Vector2(outerRectangle.X, outerRectangle.Y + (outerRectangle.Height / 2)),
+                // new Vector2(outerRectangle.X + (outerRectangle.Width / 2), outerRectangle.Y),
+                // new Vector2(outerRectangle.X + outerRectangle.Width, outerRectangle.Y + (outerRectangle.Height / 2)),
+                // new Vector2(outerRectangle.X + (outerRectangle.Width / 2), outerRectangle.Y + outerRectangle.Height),
+                // new Vector2(outerRectangle.X, outerRectangle.Y + (outerRectangle.Height / 2)),
+
+                // 8 figure
                 //new Vector2(10, 0) * 5,
                 //new Vector2(20, 0) * 5,
                 //new Vector2(30, 10) * 5,
@@ -38,12 +40,22 @@ namespace Humble
                 //new Vector2(10, 30) * 5,
                 //new Vector2(0, 20) * 5,
                 //new Vector2(0, 10) * 5,
+
+                // new 8 figure
+                new Vector2(10, 10) * 5,
+                new Vector2(25, 5) * 5,
+                new Vector2(40, 10) * 5,
+                new Vector2(45, 25) * 5,
+                new Vector2(40, 40) * 5,
+                new Vector2(25, 45) * 5,
+                new Vector2(10, 40) * 5,
+                new Vector2(5, 25) * 5,
             };
         }
 
         public Vector2 Center()
         {
-            return new Vector2(50, 50);
+            return new Vector2(125, 125);
         }
 
         public bool Contains(Vector2 player)
@@ -64,8 +76,8 @@ namespace Humble
                 Vector2 nextInnerpoint;
 
                 Vector2 actualInnerpoint = innerPoints[i];
-                int stepX = 0;
-                int stepY = 0;
+                int stepX = 1;
+                int stepY = 1;
 
                 if (i + 1 >= innerPoints.Count) {
                     nextInnerpoint = innerPoints[0];
@@ -77,40 +89,68 @@ namespace Humble
                 float difX = nextInnerpoint.X - actualInnerpoint.X;
                 float difY = nextInnerpoint.Y - actualInnerpoint.Y;
 
-                if (difX > 0)
+                if (Math.Abs(difY) > Math.Abs(difX))
                 {
-                    stepX = 1;
+                    stepY = Math.Abs((Convert.ToInt32(difY) / Convert.ToInt32(difX)));
                 }
-                else if (difX < 0)
+                if (difY < 0 )
                 {
-                    stepX = -1;
+                    stepY *= -1;
+                } else if (difY == 0)
+                {
+                    stepY = 0;
                 }
-                else {
+
+                if (Math.Abs(difX) >= Math.Abs(difY))
+                {
+                    stepX = Math.Abs((Convert.ToInt32(difX) / Convert.ToInt32(difY)));
+                }
+
+                if (difX < 0)
+                {
+                    stepX *= -1;
+                }
+                else if (difX == 0)
+                {
                     stepX = 0;
                 }
 
-                if (difY > 0)
-                {
-                    stepY = 1;
-                }
-                else if (difY < 0)
-                {
-                    stepY = -1;
-                }
-                else {
-                    stepY = 0;
-                }
                 vectorBx = new Vector2(stepX, stepY);
-                vectorB.Add(vectorBx);
+                //vectorB.Add(vectorBx);
             }
+
+            vectorB.Add(new Vector2(1, 0));
+            vectorB.Add(new Vector2(1, 1));
+            vectorB.Add(new Vector2(0, 1));
+            vectorB.Add(new Vector2(-1, 1));
+            vectorB.Add(new Vector2(-1, 0));
+            vectorB.Add(new Vector2(-1, -1));
+            vectorB.Add(new Vector2(0, -1));
+            vectorB.Add(new Vector2(1, -1));
+
+            // double lenB = Math.Sqrt(vectorB[0].X * vectorB[0].X + vectorB[0].Y * vectorB[0].Y);
+            // List<Vector2> dpx = new List<Vector2>();
+            // for (int i = 0; i < vectorB.Count; i++)
+            // {
+            //     double bX = vectorB[i].X / lenB;
+            //     double bY = vectorB[i].Y / lenB;
+            //     dpx.Add(new Vector2((float)bX, (float)bY));
+            // }
 
             List<int> dotProducts = new List<int>();
-
             for (int i = 0; i < vectorB.Count; i++)
             {
+                // int dotProduct = (int)(vectorA[i].X * dpx[i].X + vectorA[i].Y * dpx[i].Y);
                 int dotProduct = (int)(vectorA[i].X * vectorB[i].X + vectorA[i].Y * vectorB[i].Y);
+                Console.WriteLine(dotProduct);
                 dotProducts.Add(dotProduct);
             }
+
+            Console.WriteLine("------------");
+            // for(int i = 0; i < dotProducts.Count; i++)
+            // {
+            //     Console.WriteLine(dotProducts[i]);
+            // }`
 
             bool containsNegative = dotProducts.Any(i => i < 0);
 
