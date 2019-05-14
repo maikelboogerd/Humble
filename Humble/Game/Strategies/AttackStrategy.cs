16b75d3faf3c3fdcd5193325e5f379fc1dc62120
Maikel van den Boogerd <maikelboogerd@gmail.com>
2019-05-14 20:12:28 +0200

    Add sttack strategy

---
+++ Humble/Game/Strategies/AttackStrategy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humble
+{
+    public class AttackStrategy
+    {
+        private Random random = new Random();
+
+        public enum State
+        {
+            ACTIVE,
+            INACTIVE,
+        }
+
+        private State currentState;
+
+        public AttackStrategy()
+        {
+            currentState = State.INACTIVE;
+        }
+
+        public void Attack()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
